ff6db6d1a5eb9c0bc856c9effc3948f1ace951c8
김주찬 <rlawncks125@naver.com>
2024-07-02 05:22:01 +0900

    service worker 변경

--- public/sw.js
+++ public/sw.js
@@ -1,55 +1,91 @@
-const cacheName = "v1.0.0";
-/** 캐싱할 파일 확장자 리스트 */
-// const cacheFileList = [/.vue$/, /.css$/, /.mjs$/];
+const cacheName = "v1.0.4";
+// 캐시할 파일
+const cacheList = [
+  "https://res.cloudinary.com/dhdq4v4ar/image/upload/v1661371113/transparent-bg_-no-shadow-designify_wst7my.png",
+  "https://res.cloudinary.com/dhdq4v4ar/image/upload/v1664595455/back-Portfolio/mhmw6xonw2big2zwpzx4.jpg",
+  "https://res.cloudinary.com/dhdq4v4ar/video/upload/v1676287371/nuxt-shop_lkx3qp.mp4",
+  "https://www.museum.go.kr/uploadfile/ecms/visual/773161_m.jpg?_=1661443337447",
+];
+// const cacheList = [];
 
-// _nuxt/@~ , _nuxt/node_modules~ 제외
-const cacheFileList = /_nuxt\/((?!@|node_modules).)/;
-
-self.addEventListener("install", () => {
+self.addEventListener("install", (event) => {
   // 대기상태에 머무르지 않고 활성화
-  self.skipWaiting();
-  // console.log("install");
+  // self.skipWaiting();
+  console.log("install");
+
+  // // 캐시 저장
+  event.waitUntil(
+    caches
+      .open(cacheName)
+      .then((cache) => cache.addAll(cacheList))
+      .then(() => {
+        self.skipWaiting();
+      })
+  );
 });
 
 self.addEventListener("activate", (event) => {
-  // console.log("activate");
+  console.log("activate");
 
   // 작업이 마무리될떄가지 설치단계를 연장
   event.waitUntil(
     // 불필요한 캐시 지우기
-    caches.keys().then((keylist) => {
-      return Promise.all(
-        keylist.map((key) => {
-          if (key !== cacheName) {
-            console.log("지움", key);
-            return caches.delete(key);
-          }
-        })
-      );
-    })
+    caches
+      .keys()
+      .then((keylist) => {
+        return Promise.all(
+          keylist.map((key) => {
+            if (key !== cacheName) {
+              console.log("지움", key);
+              return caches.delete(key);
+            }
+          })
+        );
+      })
+      .then(() => {
+        self.clients.claim();
+      })
   );
 });
 
 self.addEventListener("fetch", (event) => {
-  // console.log("fetch", event.request.url);
+  console.log("fetch", event.request.url);
 
   const requestURL = new URL(event.request.url);
 
-  // cacheFileList.forEach((reg) => {
-  //   if (reg.test(requestURL.pathname)) {
-  //     // 캐시 저장
-  //     caches.open(cacheName).then((cache) => {
-  //       cache.add(event.request);
-  //     });
-  //   }
-  // });
+  // console.log("fetch", requestURL.pathname);
 
-  if (cacheFileList.test(requestURL.pathname)) {
-    // 캐시 저장
-    caches.open(cacheName).then((cache) => {
-      cache.add(event.request);
-    });
-  }
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      // 캐시된 응답이 있다면 제공
+      if (response) {
+        return response;
+      }
+
+      // 캐시된 응답이 없으면 네트워크 요청
+      return fetch(event.request).then((networkResponse) => {
+        // 네트워크 응답이 유효한 경우에만 캐시에 저장
+        if (
+          networkResponse &&
+          networkResponse.status === 200 &&
+          networkResponse.headers.get("Content-Length") !== "0"
+        ) {
+          return caches.open(cacheName).then((cache) => {
+            cache.put(event.request, networkResponse.clone());
+            return networkResponse;
+          });
+        } else {
+          return networkResponse;
+        }
+      });
+      // .catch(() => {
+      //   // 네트워크 요청이 실패하면 대체 콘텐츠 제공 (옵션)
+      //   if (event.request.destination === "document") {
+      //     return caches.match("/offline.html");
+      //   }
+      // });
+    })
+  );
 });
 
 // push 알람 처리
