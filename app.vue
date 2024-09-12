178f445ddcb36ae1e7f2d1cefc26680d429ad50a
김주찬 <rlawncks125@naver.com>
2024-09-12 17:45:05 +0900

    fix : seo 적용 방식 변경

--- app.vue
+++ app.vue
@@ -32,31 +32,6 @@ useHead({
   },
   meta: [
     {
-      name: "title",
-      content: "김주찬쇼핑몰포트폴리오",
-    },
-    {
-      name: "description",
-      content: "김주찬 쇼핑몰 포트폴리오 사이트 입니다.",
-    },
-
-    {
-      name: "og:title",
-      property: "og:title",
-      content: `김주찬 쇼핑몰 포트폴리오 사이트 입니다.`,
-    },
-    {
-      name: "og:description",
-      property: "og:description",
-      content: "김주찬 쇼핑몰 포트폴리오 사이트 입니다.",
-    },
-    {
-      name: "og:image",
-      property: "og:image",
-      content:
-        "https://res.cloudinary.com/dhdq4v4ar/image/upload/v1661371113/transparent-bg_-no-shadow-designify_wst7my.png",
-    },
-    {
       name: "og:type",
       property: "og:type",
       content: "website",
@@ -71,6 +46,14 @@ useHead({
       content: "XYcTcfWhtK72ndzC_OAqfQaLr_KZaZI8UjqSWNU5Ag8",
     },
   ],
+});
+
+useSeoMeta({
+  description: "김주찬 쇼핑몰 포트폴리오 사이트 입니다.",
+  ogTitle: `김주찬 쇼핑몰 포트폴리오 사이트 입니다.`,
+  ogDescription: "김주찬 쇼핑몰 포트폴리오 사이트 입니다.",
+  ogImage:
+    "https://res.cloudinary.com/dhdq4v4ar/image/upload/v1661371113/transparent-bg_-no-shadow-designify_wst7my.png",
 });
 </script>
 
